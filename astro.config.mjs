5dfff5f4eec2f20653e609cd450a3da836a9de60
MagicTeaMC <oneowo87@gmail.com>
2025-07-07 21:57:00 +0800

    better way for my website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
+  integrations: [spotlightjs()]
 });
