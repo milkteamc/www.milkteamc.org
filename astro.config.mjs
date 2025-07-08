2c0861b8c315bc3c8993cab3456a0ec14a26dea2
MagicTeaMC <oneowo87@gmail.com>
2025-07-08 12:25:40 +0800

    i dont like sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
+  site: 'https://www.milkteamc.org'
 });
